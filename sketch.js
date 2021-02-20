86f1a2b9bcc4b493e8b9661c2937d45a277726e3
Vaibhava392 <72888969+Vaibhava392@users.noreply.github.com>
2021-02-20 16:46:19 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var hr, mn, sc;
+var hrAngle, mnAngle, scAngle;
+
+function setup() {
+  createCanvas(800,400);
+  hr = hour();
+  mn = minute();
+  sc = second();
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(255,255,255);
+  scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  hrAngle = map(hr,0,12,0,360);
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+  push();
+  rotate(mnAngle);
+  stroke(0,255,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+  push();
+  rotate(hrAngle);
+  stroke(0,0,255);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+}
